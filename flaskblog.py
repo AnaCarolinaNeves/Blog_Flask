13f38644d5578f951f752537a9f68d26c24b8cb8
AnaCarolinaNeves <89168187+AnaCarolinaNeves@users.noreply.github.com>
2024-01-18 14:48:23 -0300

    setting db models

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,35 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import CadastroForm, LoginForm
 
 app = Flask(__name__)
-
 # Key gerada pelo 'secrets.token_hex(16)'
 app.config['SECRET_KEY'] = '218311eaa19b80e1bac2808bf1caef06'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# /// significa que o arquivo será criado no caminho atual do projeto
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    icone_usuario = db.Column(db.String(20), nullable=False, default='default.jpg')
+    senha = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='autor', lazy=True)
+
+    def __repr__(self):
+        return f"Usuario('{self.nome}', '{self.email}', '{self.icone_usuario}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    data_post = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    conteudo = db.Column(db.Text, nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.titulo}', '{self.data_post}')"
 
 posts = [ 
     {
