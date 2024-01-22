a0ce799c265c72f3b8d01e4cd6957438a327342a
AnaCarolinaNeves <89168187+AnaCarolinaNeves@users.noreply.github.com>
2024-01-22 17:20:50 -0300

    update routes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 posts = [ 
@@ -56,12 +56,18 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return(redirect(url_for('home')))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         flash('Login inválido. Tente novamente!', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/logout")
 def logout():
     logout_user()
-    flash('Para entrar novamente, é necessário realizar o login', 'success')
+    flash('Para entrar novamente, é necessário realizar o login', 'info')
     return redirect(url_for('home'))
+
+@app.route("/conta")
+@login_required
+def conta():
+    return render_template('conta.html', title='Conta')
